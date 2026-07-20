091deb07d961db0a1ef5c28a2846f436752c21e8
周星星 <3381580450@qq.com>
2026-07-20 21:50:41 +0800

    实现登录页的表单校验逻辑

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,6 +6,7 @@ import { ref } from 'vue'
 const form = ref({
     account: '',
     password: '',
+    agree: true,
 })
 
 // 2.准备规则对象
@@ -16,6 +17,18 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码必须在6~14个字符之间' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                // console.log(value)
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -49,8 +62,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox v-model="form.agree" size="large">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
