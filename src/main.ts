1b5a7a6ffad7c1ee375796403bc12b2d60834148
周星星 <3381580450@qq.com>
2026-07-29 22:06:04 +0800

    添加pinia-presistedstate, 实现登录持久化

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,25 @@ import './assets/main.css'
 import 'element-plus/theme-chalk/src/index.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import {lazyPlugin} from './directives'
 import { componentPlugin } from '@/components'
 
+// 导入pinia-plugin-persistedstate插件
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 // 导入初始化样式文件
 import '@/styles/common.scss'
 
 
-
-
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
